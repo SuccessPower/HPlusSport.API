82684d2c9a702173c6071d0d1d8a39ee6db8ac6a
Success Imamun <SuccessImamun@gmail.com>
2023-08-24 20:12:29 +0100

    Added PostProduct endpoint

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using HPlusSport.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HPlusSport.API.Controllers
 {
@@ -18,9 +19,32 @@ namespace HPlusSport.API.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Product> GetAllProducts()
+        public async Task<ActionResult> GetAllProducts()
         {
-            return _context.Products.ToList();
+            return Ok(await _context.Products.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult>GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return CreatedAtAction(
+                "GetProduct", 
+                new {Id = product.Id}, 
+                product);
         }
     }
 }
--- HPlusSport.API/Models/Product.cs
+++ HPlusSport.API/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace HPlusSport.API.Models
 {
@@ -10,8 +11,9 @@ namespace HPlusSport.API.Models
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public bool IsAvailable { get; set; }
+        [Required]
         public int CategoryId { get; set; }
         [JsonIgnore]
-        public virtual Category Category { get; set; }
+        public virtual Category? Category { get; set; }
     }
 }
